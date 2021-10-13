c65432c449ec5c8da5dc1fa2efbfd71a1a9e58f0
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-10-13 16:11:21 +0100

    updated function so that a number image is shown on each click event

--- 12-dice-game/01-dice-game-starting-files/index.js
+++ 12-dice-game/01-dice-game-starting-files/index.js
@@ -1,11 +1,13 @@
 // create a variable that contains a random number from 1 to 6
 // use Math.floor(Math.random())
 
-const randomNumber = Math.floor(Math.random() * 6) + 1
-console.log(randomNumber);
+// const randomNumber = Math.floor(Math.random() * 6) + 1
+// console.log(randomNumber);
 
 // select element and then change its attribute with an event listener on click
+// in order to make the number shown be random each time, we need to set the randomNumber const inside of the function so that a new random number is made each time the function is called
 const diceOne = document.querySelector(".img1");
 diceOne.addEventListener( "click", (event) => {
+  const randomNumber = Math.floor(Math.random() * 6) + 1
   event.currentTarget.setAttribute("src", `./images/dice${randomNumber}.png`)
 })
