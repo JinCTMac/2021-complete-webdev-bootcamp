747aefc48413cc007462295be5f926353d95a031
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-10-13 15:48:48 +0100

    added random number function in index.js

---
+++ 12-dice-game/01-dice-game-starting-files/index.js
@@ -0,0 +1,5 @@
+// create a variable that contains a random number from 1 to 6
+// use Math.floor(Math.random())
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+console.log(randomNumber);
