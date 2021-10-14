b55857bdc677c70a8ac004cf681c33634a4ba4fe
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-10-14 17:47:38 +0100

    redefining logic for selecting dice for winning or losing

--- 12-dice-game/01-dice-game-starting-files/index.js
+++ 12-dice-game/01-dice-game-starting-files/index.js
@@ -7,15 +7,19 @@
 // select element and then change its attribute with an event listener on click
 // in order to make the number shown be random each time, we need to set the randomNumber const inside of the function so that a new random number is made each time the function is called
 const diceOne = document.querySelector(".img1");
+const diceTwo = document.querySelector(".img2");
+
 diceOne.addEventListener( "click", (event) => {
   const randomNumber = Math.floor(Math.random() * 6) + 1
   event.currentTarget.setAttribute("src", `./images/dice${randomNumber}.png`)
+  diceTwo.setAttribute("src", `./images/dice${randomNumber}.png`)
 })
 
 // same thing for second dice
 
-const diceTwo = document.querySelector(".img2");
-diceTwo.addEventListener( "click", (event) => {
-  const randomNumber = Math.floor(Math.random() * 6) + 1
-  event.currentTarget.setAttribute("src", `./images/dice${randomNumber}.png`)
-})
+// diceTwo.addEventListener( "click", (event) => {
+//   const randomNumber = Math.floor(Math.random() * 6) + 1
+//   event.currentTarget.setAttribute("src", `./images/dice${randomNumber}.png`)
+// })
+
+// actually, would be better and easier to make dice 2 auto update upon dice one being clicked
