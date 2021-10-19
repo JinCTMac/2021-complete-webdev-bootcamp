568caec12c56e5571c843ca533eba0cd88f5c8d9
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-10-19 17:44:01 +0100

    adding button animations

--- 13-advanced-JS-DOM/01-drum-starter/index.js
+++ 13-advanced-JS-DOM/01-drum-starter/index.js
@@ -1,71 +1,84 @@
-// Adding event listeners to buttons
+// JS code for drum kit
 
-// function that will happen when a button gets clicked on/keyup on
+// const clickHandler = () => {
+//   // alert("get clicked!");
+//   // const audio = new Audio("./sounds/tom-1.mp3");
+//   // audio.play();
+//   // we need to use the this keyword to refer to the specific key being clicked
+//   // for some reason, cannot access this keyword in this function
+//   // likely because it doesn't pass on the data for this to the function
+// }
 
-const clickHandler = () => {
-  // alert("get clicked!");
-  // const audio = new Audio("./sounds/tom-1.mp3");
-  // audio.play();
-  // we need to use the this keyword to refer to the specific button being clicked
-  // for some reason, cannot access this keyword in this function
-  // likely because it doesn't pass on the data for this to the function
+const keyCheck = (key) => {
+    switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+    default: console.log(key);
+  }
 }
 
-// allButtons.forEach((button) => {
-//   button.addEventListener('click', (event) => {
-//     clickHandler();
-//   })
-// })
+// animation that occurs when key is pressed
 
-// 1. Eventlistener for buttons
+const buttonAnimation = (key) => {
+  const pressedKey = document.querySelector(`.${key}`)
+  pressedKey.classList.toggle("pressed");
+}
 
-const allButtons = document.querySelectorAll('.drum');
+// Eventlisteners
+
+const allKeys = document.querySelectorAll('.drum');
 
 // console.log(this) only works with an old fashioned function for some reason
-allButtons.forEach((button) => {
-  button.addEventListener('click', function() {
+allKeys.forEach((key) => {
+  // for clicks
+  key.addEventListener('click', function(event) {
     // console.log(this);
-    // this.style.color = "white";
+    const key = this.innerHTML;
+    keyCheck(key);
+    buttonAnimation(key);
 
-    const buttonInnerHTML = this.innerHTML;
+  })
 
-    switch (buttonInnerHTML) {
-      case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case "a":
-        var tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case "s":
-        var tom3 = new Audio('sounds/tom-3.mp3');
-        tom3.play();
-        break;
-
-      case "d":
-        var tom4 = new Audio('sounds/tom-4.mp3');
-        tom4.play();
-        break;
-
-      case "j":
-        var snare = new Audio('sounds/snare.mp3');
-        snare.play();
-        break;
-
-      case "k":
-        var crash = new Audio('sounds/crash.mp3');
-        crash.play();
-        break;
-
-      case "l":
-        var kick = new Audio('sounds/kick-bass.mp3');
-        kick.play();
-        break;
-
-      default: console.log(key);
-    }
+  // for keyups
+  key.addEventListener('keyup', function(event) {
+    const key = this.innerHTML;
+    // we need to use event.key, so that it searches for the keyup event
+    // because the event object has a key property, which tells us which key was pressed
+    // if we just used key instead of event.key, it would respond to all key presses of any key
+    keyCheck(event.key)
+    buttonAnimation(event.key);
   })
 });
