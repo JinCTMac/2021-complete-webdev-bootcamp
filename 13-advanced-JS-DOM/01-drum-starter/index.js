2d3d183fb8440d2478cc9f8a05ff2aad8d229b19
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-10-19 15:24:49 +0100

    adding eventlisteners to each button and callback function in ES6

--- 13-advanced-JS-DOM/01-drum-starter/index.js
+++ 13-advanced-JS-DOM/01-drum-starter/index.js
@@ -1,1 +1,20 @@
 // Adding event listeners to buttons
+
+// function that will happen when a button gets clicked on/keyup on
+
+const clickHandler = () => {
+  // alert("get clicked!");
+  const audio = new Audio("./sounds/tom-1.mp3");
+  audio.play();
+}
+
+
+// all buttons
+
+const allButtons = document.querySelectorAll('.drum');
+
+allButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    clickHandler();
+  })
+})
