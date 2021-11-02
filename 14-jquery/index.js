6c330fed730079c87197adfc6feef5f52a72f3d8
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-11-02 17:19:45 +0000

    adding style via jquery

--- 14-jquery/index.js
+++ 14-jquery/index.js
@@ -1,6 +1,11 @@
 // jQuery practice
 // shortens code
 
-$(document).ready(() => {
-  $("h1").css("color", "red")
-})
+// this code below is only necessary if we are putting the CDN in the head
+// $(document).ready(() => {
+//   $("h1").css("color", "red")
+// })
+
+$("h1").css("color", "red")
+
+console.log($("h1").css("color"))
