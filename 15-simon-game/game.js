447991872975a83947019fd06e56dad5ffca6741
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-11-04 17:55:42 +0000

    added playsound function and passed in the right arguments for them to work on button click

--- 15-simon-game/game.js
+++ 15-simon-game/game.js
@@ -3,13 +3,25 @@ const buttonColours = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
 const userClickedPattern = [];
 
-const clickhandler = (value) => {
+// detect when any button is clicked
+const button = $(".btn");
+
+button.on("click", () => {
   // we get the id of the button the user clicks on
   // and then we add that to the userClickedPattern array
   // and check if that is the same as the gamePattern
   // if not, then we tell player they lost
-  const userChosenColour = value.target.id;
-}
+
+  // to get the id of the button the user clicked on, we can use button.attr("id") to get the value of the id
+  const userChosenColour = button.attr("id");
+
+  // then we push to the array
+  userClickedPattern.push(userChosenColour)
+
+  // we call the playSound function that takes the ID of the button clicked to parse the right audio file
+  playSound(userChosenColour);
+})
+
 
 // nextSequence function
 // controls the next sequence given in the simon game
@@ -17,24 +29,28 @@ const clickhandler = (value) => {
 const nextSequence = () => {
   // to make a random number, we need Math.floor(Math.random())
   // where Math.floor rounds the number down and Math.random() makes a random num between 0 and 1(upperbound)
-  // we multiply by 3 and add 1 to ensure only positive numbers above 0
-  const randomNumber = Math.floor(Math.random() * 3) + 1
+  // we multiply by 4 to allow it to select a value between 0 and 3 corresponding to index
+  const randomNumber = Math.floor(Math.random() * 4)
   // we define a random chosen colour by indexing a random value from a defined array of colours, buttonColours
   const randomChosenColour = buttonColours[randomNumber]
   // then to define the sequence of colours, we append it to another array, holding the sequence of colours gamePattern
   gamePattern.push(randomChosenColour)
 
   // selecting button with same id as randomChosenColour
-  const chosenButton = $(`#${randomChosenColour}`);
+  const chosenButton = $("#" + randomChosenColour);
   // flash animation
   chosenButton.fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-  // setting sound object to be equal to the sound of the button
-  const audioObject = new Audio(`./sounds/${randomChosenColour}.mp3`);
+
+  //playing sound
+  playSound(chosenButton)
+
+}
+
+const playSound = (name) => {
+  // we take the name as the value of the id from the clicked button
+  // we call this playSound() function in the event listener for the button click
+  // so on click, the ID of the button is passed into this function, which plays the according sound
+  const audioObject = new Audio(`./sounds/${name}.mp3`);
   // play audio
   audioObject.play();
-
-  // detect when button is clicked
-  chosenButton.on("click", (event) => {
-    clickHandler(event);
-  })
 }
