3ac52a636dff0e3252b84df946a3be858f1577a8
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-11-04 17:09:36 +0000

    building initial logic for choosing colours from array, then adding it to a sequence to check against

--- 15-simon-game/game.js
+++ 15-simon-game/game.js
@@ -1,1 +1,15 @@
-alert("WOW")
+
+const buttonColours = ["red", "blue", "green", "yellow"];
+const gamePattern = [];
+
+// nextSequence function
+// controls the next sequence given in the simon game
+// i.e. when you get it correct, randomly selects another colour and adds to sequence
+const nextSequence = () => {
+  // to make a random number, we need Math.floor(Math.random())
+  // where Math.floor rounds the number down and Math.random() makes a random num between 0 and 1(upperbound)
+  // we multiply by 3 and add 1 to ensure only positive numbers above 0
+  const randomNumber = Math.floor(Math.random() * 3) + 1
+  const randomChosenColour = buttonColours[randomNumber]
+  gamePattern.push(randomChosenColour)
+}
