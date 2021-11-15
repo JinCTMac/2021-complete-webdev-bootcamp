fd859ca2323c3aafd9908e101684fd0204ffe5bf
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-11-15 16:34:16 +0000

    external node packages testing

--- 18-node-intro/node_intro/node_intro.js
+++ 18-node-intro/node_intro/node_intro.js
@@ -1,13 +1,18 @@
+// LESSON 1 - BUILT-IN NODE PACKAGES
+
 // imported from nodejs.org website
 // import * as fs from 'fs/promises';
 // import * as fs from 'fs';
 
-const fs = require('fs');
+// const fs = require('fs');
 
 // taken from stackOverflow - https://stackoverflow.com/questions/11293857/fastest-way-to-copy-a-file-in-node-js
-fs.copyFile('file1.txt', 'file2.txt', (err) => {
-  if (err) throw err;
-  console.log('source.txt was copied to destination.txt');
-});
+// fs.copyFile('file1.txt', 'file2.txt', (err) => {
+//   if (err) throw err;
+//   console.log('source.txt was copied to destination.txt');
+// });
 
-// fs.copyFileSync("file1.text", "file2.txt");
+// INFO - why package.json is needed and what it does
+// https://heynode.com/tutorial/what-packagejson/#:~:text=Recap-,The%20package.,entry%20point%20to%20our%20package.
+
+// LESSON 2 - EXTERNAL NODE PACKAGES FROM NPM
