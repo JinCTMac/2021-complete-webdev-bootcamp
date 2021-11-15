227900275e3953750caaf1022e6043ac60aef745
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-11-15 17:15:25 +0000

    comments for extra explanation about the server function

--- 19-express/my-express-server/server.js
+++ 19-express/my-express-server/server.js
@@ -9,10 +9,12 @@ const app = express();
 // Step 3 - we set the port for localhost equal to 3000
 const port = 3000;
 
+// forms the body of the page - when we access the root URL, /, it will return a response of 'Hello World!', via a get request
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// Step 4 - a callback function that tells us that our app is listening onto the port we specified
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
