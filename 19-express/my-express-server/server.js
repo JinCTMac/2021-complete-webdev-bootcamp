294929345a8c08b0e82e433e44ea32d1498ced34
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-11-21 17:07:45 +0000

    adding HTML response

--- 19-express/my-express-server/server.js
+++ 19-express/my-express-server/server.js
@@ -9,12 +9,14 @@ const app = express();
 // Step 3 - we set the port for localhost equal to 3000
 const port = 3000;
 
-// forms the body of the page - when we access the root URL, /, it will return a response of 'Hello World!', via a get request
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 // Step 4 - a callback function that tells us that our app is listening onto the port we specified, which will show up as a console.log inside of our terminal
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+// forms the body of the page - when we access the root URL, /, it will return a response of 'Hello World!', via a get request
+// this is what happens when a browser makes a get request
+// we can also send back HTML wrapped in quotes instead of just plain text
+app.get('/', (req, res) => {
+  res.send("<h1>Hello World!</h1>")
 })
